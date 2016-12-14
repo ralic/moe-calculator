2e81dfb3ed8debece9d2a98a6521e2dc4e0c76d4
Ralic Lo <raliclo@gmail.com>
2016-12-13 20:27:14 -0800

    Updates

--- ios/build/moe/xcodebuild/obj/Calculator.build/Debug-iphonesimulator/Calculator.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/raliclo/work/proj/moe-ex/moe-samples-java/Calculator/ios/build/xcode/Calculator/main.cpp \
-  /Users/raliclo/work/proj/moe-ex/moe-samples-java/Calculator/ios/build/xcode/../../build/moe/sdk/sdk/iphonesimulator/MOE.framework/Headers/MOE.h
